5fbb5d5ef5d1183f1c53f28dc71b70e67a9fb4bc
tomimandalap <tommymandalaputra@gmail.com>
2021-02-12 00:04:10 +0700

    header baru

--- src/components/headers.vue
+++ src/components/headers.vue
@@ -18,7 +18,49 @@
           <router-link class="nav-link" id="link4" to="/">History</router-link>
         </div>
       </div>
-      <div class="justify-conten-end">
+      <div v-if="getAccess == 2" class="justify-conten-end">
+        <button
+          @click="search()"
+          style="
+            border: none;
+            background: none;
+            font-size: 24px;
+            color: #4f5665;
+          "
+          class="mr-5"
+        >
+          <i class="fas fa-search"></i>
+        </button>
+        <button
+          @click="chating()"
+          style="
+            border: none;
+            background: none;
+            font-size: 30px;
+            color: #4f5665;
+          "
+          class="mr-4"
+        >
+          <span
+            class="font-rubik mt-n1 ml-n3"
+            style="
+              position: absolute;
+              padding: 5px 10px;
+              background: #6a4029;
+              font-size: 8px;
+              font-weight: 600;
+              color: #fff;
+              border-radius: 50%;
+            "
+            >1</span
+          >
+          <img src="../assets/img/chat.png" alt="chat" />
+        </button>
+        <button @click="signout()" style="background: none; border: none">
+          <img src="../assets/img/profile1.png" alt="" />
+        </button>
+      </div>
+      <div v-else>
         <button @click="login()" class="btn btn-link mr-4 bt-login">
           Login
         </button>
@@ -31,8 +73,25 @@
   <!-- end navbar -->
 </template>
 <script>
+import { mapGetters, mapActions } from 'vuex'
 export default {
+  computed: {
+    ...mapGetters({
+      getAccess: 'auth/getAccess'
+    })
+  },
   methods: {
+    ...mapActions({
+      logout: 'auth/logout'
+    }),
+    signout () {
+      // alert('signout')
+      this.logout().then((response) => {
+        if (response) {
+          this.$router.push('/')
+        }
+      })
+    },
     login () {
       // alert('Login')
       this.$router.push('/login')
@@ -40,6 +99,12 @@ export default {
     signup () {
       // alert('Signup')
       this.$router.push('/signup')
+    },
+    chating () {
+      alert('chating')
+    },
+    search () {
+      alert('Searching')
     }
   }
 }
--- src/components/w-productadmin.vue
+++ src/components/w-productadmin.vue
@@ -13,7 +13,7 @@
         <section class="box-aside-menu" id="aside-menu3">KETIGA</section>
         <section class="box-aside-menu" id="aside-menu2">KEDUA</section>
         <section class="box-aside-menu" id="aside-menu1">
-          <span @click="test()" class="icon-edit-cupon"
+          <span v-if="getAccess == 1" @click="test()" class="icon-edit-cupon"
             ><i class="fas fa-pencil-alt"></i
           ></span>
           <img
@@ -46,7 +46,11 @@
             </li>
           </ul>
           <!-- button admin -->
-          <button @click="addpromo()" class="btn btn-warning bt-addpromo">
+          <button
+            v-if="getAccess == 1"
+            @click="addpromo()"
+            class="btn btn-warning bt-addpromo"
+          >
             Add New Promo
           </button>
         </section>
@@ -87,7 +91,10 @@
                 <h6 class="card-subtitle-menus mb-2">
                   IDR {{ formatPrice(element.price) }}
                 </h6>
-                <span @click="editProduct(element.id)" class="icon-edit-menus"
+                <span
+                  v-if="getAccess == 1"
+                  @click="editProduct(element.id)"
+                  class="icon-edit-menus"
                   ><i class="fas fa-pencil-alt"></i
                 ></span>
               </div>
@@ -100,7 +107,7 @@
     </div>
     <section class="button-add">
       <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-12" v-if="getAccess == 1">
           <button @click="addnewproduct()" class="btn btn-add-new-product">
             Add New Product
           </button>
@@ -129,6 +136,7 @@ export default {
   },
   computed: {
     ...mapGetters({
+      getAccess: 'auth/getAccess',
       allitems: 'products/allitems'
     })
   },
